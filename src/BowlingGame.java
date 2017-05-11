18a436b490f80052b907de7415f2b3c8974e34a8
somename <someemail>
2017-05-11 20:34:53 +0200

    besouro automatic message

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -9,22 +9,44 @@ public class BowlingGame {
 	//a bowling game is made of (at least) 10 frames
 	private List<Frame> frames = new ArrayList<Frame>();
 	private Frame bonus;
+	private int bonusGame =0;
 	
 	public BowlingGame(){}
 	
 	// adds a frame to the game
-	public void addFrame(Frame frame){
+	public void addFrame(Frame frame) throws BowlingException
+	{
+		if(frames.size()==10)
+			throw new BowlingException();
+		else
+			frames.add(frame);
+			
 		//to be implemented
+		
+		
 	}
 	
 	// Sets the bonus throws at the end of the game
 	public void setBonus(int firstThrow, int secondThrow) {
 		//to be implemented
+		for(int i=0; i<10; i++)
+			if(frames.get(i).isSpare())
+			{
+				bonus= frames.get(i+1);
+				bonusGame+=bonus.score();
+			}
+			else if(frames.get(i).isStrike())
+			{
+				bonus = frames.get(i+1);
+				bonusGame+=bonus.score();
+				
+			}
 	}
 	
 	// Returns the game score
 	public int score(){
 		//to be implemented: should return game score 
+		
 		return 0;
 	}
 }
