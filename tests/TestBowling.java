88dd43203b80b4e9aaf1b279dbb4f3ab25d655a6
somename <someemail>
2017-05-11 20:34:56 +0200

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -5,8 +5,54 @@ import org.junit.Test;
 public class TestBowling {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_firstThrow_secondThrow_isSpare() throws BowlingException {
+		
+		Frame frame = new Frame(5,5);
+		
+		assertEquals ("First throw and second throw", "10", frame);
+		frame.getFirstThrow();
+		frame.getFirstThrow();
+		frame.getFirstThrow();
+		frame.getFirstThrow();
+		frame.getFirstThrow();
+		
+		
+		
 	}
+	
+	public void test_firstThrow_secontThrow_notSpare() throws BowlingException {
+		Frame frame = new Frame(3,5);
+		assertEquals ("First throw and second throw", "", frame);
+		
+	}
+	@Test(expected=BowlingException.class)
+	public void test_firstThrow_isStrike() throws BowlingException {
+		
+		Frame frame = new Frame(10,0);
+		BowlingGame game = new BowlingGame();
+		game.addFrame(frame);
+		game.addFrame(frame);
+		game.addFrame(frame);
+		game.addFrame(frame);
+		game.addFrame(frame);
+		game.addFrame(frame);
+		game.addFrame(frame);
+		game.addFrame(frame);
+		game.addFrame(frame);
+		game.addFrame(frame);
+		assertEquals ("First throw and second throw", "", frame);
+	}
+	
+	public void test_firstThrow_notStrike() throws BowlingException {
+		
+		Frame frame = new Frame(0,1);
+		assertEquals ("First throw and second throw", "", frame);
+	}
+	
+	
+	
+	
+	
+	
 
 }
